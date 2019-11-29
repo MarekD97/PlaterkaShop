5adf847492114d71dcecdeb407fc8028a800de7c
MarekD97 <mdorosz2@wp.pl>
2019-11-29 05:49:41 +0100

    Add subpage for not found pages

---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <div className="header">
+                    <h1>Strona nie odnaleziona</h1>
+                </div>
+            </div>);
+    }
+};
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
@@ -8,13 +8,17 @@ import Main from './components/Main.js';
 import List from './components/List.js';
 import Item from './components/Item.js';
 import Contact from './components/Contact.js';
+import NotFound from './components/NotFound.js';
 
 ReactDOM.render(
     <Router>
         <Main>
-            <Route exact path="/" component={List} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/product/:value" component={Item} />
+            <Switch>
+                <Route exact path="/" component={List} />
+                <Route path="/contact" component={Contact} />
+                <Route path="/product/:value" component={Item} />
+                <Route exact path='*' component={NotFound} />
+            </Switch>
         </Main>
     </Router>,
     document.getElementById('root'));
