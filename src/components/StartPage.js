376226e31182d3b0a83466af1c7e9a4713b080f8
MarekD97 <mdorosz2@wp.pl>
2020-01-11 20:45:19 +0100

    Add image in start page

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -12,12 +12,18 @@ const styles = {
         flexFlow: 'row wrap',
         justifyContent: 'center',
         height: '80vh'
+    },
+    logo: {
+        width: "auto",
+        height: window.innerWidth < 576 ? '40%' : '60%',
+        objectFit: 'contain'
     }
 }
 
 const StartPage = () => {
     return (
         <div style={styles.container} >
+            <img src={'./images/platerkapodstawa2 (2).png'} alt='logo Platerki' style={styles.logo} />
             <h1 style={styles.header}>Witamy na oficjalnej stronie sklepu II LO im. Emilii Plater w Białej Podlaskiej. Zapraszamy do zapoznania się z naszą aktualną ofertą</h1>
         </div >);
 };
