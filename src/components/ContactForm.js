f00090d78ed125f0af32adf5402eca6b35134178
MarekD97 <mdorosz2@wp.pl>
2020-03-03 13:43:12 +0100

    Change contact form email

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -55,7 +55,7 @@ class ContactForm extends React.Component {
                 <form
                     style={styles.form}
                     onSubmit={this.submitForm}
-                    action="https://formspree.io/meqkrwwd"
+                    action="https://formspree.io/mzbgkazd"
                     method="POST"
                 >
                     <label>Imię:</label>
